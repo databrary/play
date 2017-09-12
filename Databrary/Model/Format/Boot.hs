6924dc88cbe8b8d81519b10e1fa115f80060bf85
Kanishka Azimi <kanishka@192.168.1.7>
2017-09-12 16:24:07 -0400

    Start to remove Selector from Format model

--- Databrary/Model/Format/Boot.hs
+++ Databrary/Model/Format/Boot.hs
@@ -9,9 +9,14 @@ import qualified Language.Haskell.TH.Syntax as TH
 
 import Databrary.Service.DB
 import Databrary.Model.SQL (selectQuery)
+import Databrary.Model.SQL.Select
 import Databrary.Model.Format.SQL
 
 loadFormats :: TH.ExpQ
 loadFormats = do
-  l <- runTDB $ dbQuery $(selectQuery formatRow "ORDER BY id")
+  l <- runTDB 
+         $ dbQuery 
+            $(selectQuery 
+                (selectColumns 'makeFormat "format" ["id", "mimetype", "extension", "name"]) 
+                "ORDER BY id")
   TH.lift l
