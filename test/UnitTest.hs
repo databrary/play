bae033c8c9bfdffabd20dafb29e0247a2ea5f211
kanishka <kanishka.azimi@databrary.org>
2017-10-01 07:17:33 -0400

    Add library, minimal test-suite

---
+++ test/UnitTest.hs
@@ -0,0 +1,27 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Data.RangeSet.Parse
+import qualified Data.RangeSet.List as R
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests = testGroup "Data.RangeSet.Parse"
+  [ testCase "parseRangeSet-1"
+    (parseRangeSet "" @?= (Just (R.fromList [] :: R.RSet Bool)))
+  , testCase "parseRangeSet-2"
+    (parseRangeSet "1-10" @?= (Just (R.fromRangeList [(1, 10)] :: R.RSet Int)))
+  , testCase "parseRangeSet-3"
+    (parseRangeSet "1-10,20-30"
+       @?= (Just (R.fromRangeList [(1, 10), (20, 30)] :: R.RSet Int)))
+  , testCase "showRangeSet-1"
+    (showRangeSet (R.fromRangeList ([] :: [(Int,Int)]))
+       @?= "")
+  , testCase "showRangeSet-2"
+    (showRangeSet (R.fromRangeList ([(1, 10)] :: [(Int,Int)]))
+       @?= "1-10")
+  ]
