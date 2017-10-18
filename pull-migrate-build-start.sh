16a79f0b3acfc671fa3f1388d86a8ab94859dd1e
kanishka <kanishka.azimi@databrary.org>
2017-10-18 18:47:55 -0400

    Absolute paths

--- pull-migrate-build-start.sh
+++ pull-migrate-build-start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 build_user="centos"
-clone_path="~$build_user/src/databrary"
-exe_dir="~$build_user/.cabal/bin"
-data_basedir="~$build_user/.cabal/share/x86_64-linux-ghc-7.10.3"
+clone_path="/home/$build_user/src/databrary"
+exe_dir="/home/$build_user/.cabal/bin"
+data_basedir="/home/$build_user/.cabal/share/x86_64-linux-ghc-7.10.3"
 branch="develop"
 
 cd $clone_path
