5146aa58c1fe51bea94404b700bfb70663893c97
kanishka <kanishka.azimi@databrary.org>
2017-10-18 19:10:53 -0400

    Fix typo

--- pull-migrate-build-start.sh
+++ pull-migrate-build-start.sh
@@ -8,7 +8,7 @@ data_basedir="/home/$build_user/.cabal/share/x86_64-linux-ghc-7.10.3"
 cd $clone_path
 branch=`cat ../databrary-branch.txt`
 echo "=== Check branch matches required branch in ../databrary-branch.txt"
-$current_branch=`git rev-parse --abbrev-ref HEAD`
+current_branch=`git rev-parse --abbrev-ref HEAD`
 echo "Current branch is $current_branch."
 
 echo "=== Current working copy status"
