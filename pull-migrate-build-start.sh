86fb5bb07269a795c0802bc6170e4c1f4adde848
kanishka <kanishka.azimi@databrary.org>
2017-10-18 19:08:03 -0400

    Improve, move automation

--- pull-migrate-build-start.sh
+++ pull-migrate-build-start.sh
@@ -4,11 +4,12 @@ build_user="centos"
 clone_path="/home/$build_user/src/databrary"
 exe_dir="/home/$build_user/.cabal/bin"
 data_basedir="/home/$build_user/.cabal/share/x86_64-linux-ghc-7.10.3"
-branch="develop"
 
 cd $clone_path
+branch=`cat ../databrary-branch.txt`
 echo "=== Check branch matches required branch in ../databrary-branch.txt"
-
+$current_branch="git rev-parse --abbrev-ref HEAD"
+echo "Current branch is $current_branch."
 
 echo "=== Current working copy status"
 git status
@@ -19,8 +20,9 @@ git pull
 
 echo "=== Run new db migrations, build, install"
 ./dev
-builtexe=`ls -lt $exe_dir/databrary-* | head -1` #specify this to a specific version
+builtexe=`ls -lt $exe_dir/databrary-* | head -1` #extract exact version from git describe instead
 
 echo "=== Start"
 ls databrary.conf
 databrary_datadir="$data_basedir/databrary-1" `$builtexe`
+echo "... Started!"
