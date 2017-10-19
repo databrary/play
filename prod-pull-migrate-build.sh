03c94807ca513ff9b2703f4ab645b8feabefdce5
kanishka <kanishka.azimi@databrary.org>
2017-10-19 13:31:32 -0400

    Duplicate stage scripts for prod
    - intentionally duplicate for now, to avoid any possible confusion

---
+++ prod-pull-migrate-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+build_user="build"
+clone_path="/home/$build_user/databrary"
+exe_dir="/home/$build_user/.cabal/bin"
+
+echo "=== Changing to repository root directory $clone_path"
+cd $clone_path
+
+echo "=== Check branch matches required branch"
+branch="master"
+current_branch=`git rev-parse --abbrev-ref HEAD`
+echo "Current branch is $current_branch."
+if [ "$branch" != "$current_branch" ]
+then
+  echo "Expected branch to be $branch. Please correct."
+  exit 1
+fi
+
+echo "=== Current working copy status"
+git status
+
+echo "=== Stash, pull latest from $branch"
+git stash save # need in case there were manual, conflicting changes that would prevent pulling from succeeding
+git pull
+
+echo "=== Run new db migrations, build, install"
+./dev
+built_exe=`ls -t $exe_dir/databrary-* | head -1` #extract exact version from git describe instead
+echo "Newest installed exe found is: $built_exe"
+
