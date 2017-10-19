d0528bd9f1e83b5a1ea01f702731c4cf4fbb034f
kanishka <kanishka.azimi@databrary.org>
2017-10-19 10:00:31 -0400

    Remove whitespace from read branch

--- pull-migrate-build-start.sh
+++ pull-migrate-build-start.sh
@@ -8,7 +8,7 @@ data_basedir="/home/$build_user/.cabal/share/x86_64-linux-ghc-7.10.3"
 cd $clone_path
 echo "=== Check branch matches required branch in ../databrary-branch.txt"
 echo "Reading branch from ../databrary-branch.txt"
-branch=`cat ../databrary-branch.txt` # trim whitespace
+branch=`cat ../databrary-branch.txt | tr -d '[:space:]'` # trims any whitespace char
 current_branch=`git rev-parse --abbrev-ref HEAD`
 echo "Current branch is $current_branch."
 if [ "$branch" != "$current_branch" ]
@@ -20,7 +20,7 @@ fi
 echo "=== Current working copy status"
 git status
 
-echo "=== Stash, pull latest"
+echo "=== Stash, pull latest from $branch"
 git stash save # need in case there were manual, conflicting changes to prevent pulling from succeeding
 git pull
 
