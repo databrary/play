d454821aceb2e42a6a648bcc994c72d1048c4d05
hsloan <ishaqsloan@gmail.com>
2017-10-19 12:14:21 -0400

    db initialization cleanup

--- initializeDB.sh
+++ initializeDB.sh
@@ -3,13 +3,6 @@ set -x
 dbName=${1:-databrary-nix-db}
 dbPath=$dbName
 echo $dbPath
-gargoyle-psql "$dbPath" <<-EOSQL
-     CREATE USER databrary;
-     CREATE DATABASE databrary;
-     GRANT ALL PRIVILEGES ON DATABASE databrary TO databrary;
-     ALTER USER databrary WITH PASSWORD 'databrary123';
-     ALTER USER databrary WITH SUPERUSER;
-EOSQL
 chmod 700 $dbPath/work
 # load the schema into the db
 cat ./schema/* | gargoyle-psql "$dbPath"
