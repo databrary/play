c05a6413a0cde306fd5aac0b2646ae31f5a35fe4
Luigy Leon <luigy@outlook.com>
2017-10-21 19:50:31 -0400

    Clean up shell.nix
    Latest reflex-platform has better .env behavior
    making this unecessary

--- shell.nix
+++ shell.nix
@@ -1,13 +1,1 @@
-let
-#bring into scope output variables from build.nix
-inherit (import ./. {}) pkgs nixpkgs nodePackages postgresql;
-in
-# override buildInput attribute during mkDerivation process
-pkgs.databrary-dev.env.overrideAttrs (attrs: {
-  buildInputs = attrs.buildInputs or [] ++
-    [nodePackages.shell.nodeDependencies
-     nixpkgs.nodejs-8_x
-     nixpkgs.jdk #required by Solr
-     postgresql.postgres
-    ];
-})
+(import ./. {}).pkgs.databrary-dev.env
