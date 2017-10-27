55ca17d7a68b864a1992eddbd471e067faa5ed98
kanishka <kanishka.azimi@databrary.org>
2017-10-27 17:04:10 -0400

    Start unit test Databrary.Model.Time

--- test/unit/UnitTest.hs
+++ test/unit/UnitTest.hs
@@ -2,6 +2,7 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import qualified Data.RangeSet.ParseTest
+import qualified Databrary.Model.TimeTest
 import qualified Databrary.StringTest
 import qualified Databrary.OpsTest
 
@@ -11,6 +12,7 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "Unit" 
   [ Data.RangeSet.ParseTest.tests
+  , Databrary.Model.TimeTest.tests
   , Databrary.StringTest.tests
   , Databrary.OpsTest.tests
   ]
