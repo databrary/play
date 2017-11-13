84271759a6e5af1bfba24fd33193d59619b915d9
kanishka <nish2575@gmail.com>
2017-11-13 17:58:19 -0500

    Revert back a few more to reflect some early changes to Databrary.Web and node usage

--- Databrary/Web/Stylus.hs
+++ Databrary/Web/Stylus.hs
@@ -1,23 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Databrary.Web.Stylus
-  ( generateStylusCSS
-  ) where
+    ( generateStylusCSS
+    ) where
 
 import Control.Monad.IO.Class (liftIO)
+import Control.Exception
+import qualified Data.Text as T
 import System.Process (callProcess)
 import System.FilePath (takeExtensions)
+import System.Process (readCreateProcess, shell)
+import System.Posix.Files (createSymbolicLink)
 
-import Paths_databrary.Node
 import Databrary.Files
 import Databrary.Web
 import Databrary.Web.Types
 import Databrary.Web.Files
 import Databrary.Web.Generate
+import Data.Monoid
 
 generateStylusCSS :: WebGenerator
 generateStylusCSS fo@(f, _) = do
   let src = "app.styl"
+  -- liftIO $ print $ webFileAbs f <> " " <> webFileAbs src
   sl <- liftIO $ findWebFiles ".styl"
   webRegenerate
-    (callProcess (binDir </> "stylus") $ (if takeExtensions (webFileRel f) == ".min.css" then ("-c":) else id) ["-u", "nib", "-u", "autoprefixer-stylus", "-o", webFileAbs f, webFileAbs src])
-    [] sl fo
+    (callProcess
+      "stylus" $
+    (if takeExtensions (webFileRel f) == ".min.css" then ("-c":) else id)
+    [ "-u", "nib"
+    , "-u", "autoprefixer-stylus"
+    , "-o", webFileAbs f
+    , webFileAbs src
+    ])
+    []
+    sl
+    fo
