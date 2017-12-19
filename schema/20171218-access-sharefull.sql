20f2af4d638431b74368a132d1e9aa2e90df9d01
kanishka <nish2575@gmail.com>
2017-12-19 12:02:24 -0500

    Migrate existing volume_access to everybody, add constraints
    - assume all current volume_access granted to everybody with level public has share_full of true
    - constrain volume_access to require share_full false when party is not everybody
    - constrain volume_access to have a value when party is everbody and level is public

--- schema/20171218-access-sharefull.sql
+++ schema/20171218-access-sharefull.sql
@@ -1,12 +1,13 @@
 alter table "volume_access" add column "share_full" boolean null;
 alter table audit."volume_access" add column "share_full" boolean null;
 
-
 -- migrate all existing entries such that share_full = true when party is everybody
+update volume_access
+set share_full = true
+where (party, individual, children) = (-1, 'PUBLIC', 'PUBLIC');
 
-
-
--- check constraint that value is not null when party is everbody?
+ALTER TABLE "volume_access" ADD CONSTRAINT "volume_access_check_full1" CHECK ( party = -1 OR share_full is null );
+ALTER TABLE "volume_access" ADD CONSTRAINT "volume_access_check_full2" CHECK ( party != -1 OR individual != 'PUBLIC' OR share_full is not null );
 
 CREATE OR REPLACE VIEW "volume_access_view" ("volume", "party", "access", "share_full") AS
 	SELECT volume, party, individual, share_full FROM volume_access
