70cc0ef97ce667e16694e36b39c8c7728aa66007
kanishka <nish2575@gmail.com>
2017-12-19 10:58:49 -0500

    Modify volume_access_view to propagate value of share_full
    - share_full is only set for everybody group, remaining parties will
    receive the value if they are accessing through parent group everybody

--- schema/20171218-access-sharefull.sql
+++ schema/20171218-access-sharefull.sql
@@ -4,4 +4,19 @@ alter table audit."volume_access" add column "share_full" boolean null;
 
 -- migrate all existing entries such that share_full = true when party is everybody
 
+
+
 -- check constraint that value is not null when party is everbody?
+
+CREATE OR REPLACE VIEW "volume_access_view" ("volume", "party", "access", "share_full") AS
+	SELECT volume, party, individual, share_full FROM volume_access
+
+        UNION ALL
+
+        SELECT volume
+             , child
+             , MAX(LEAST(children, CASE WHEN children <= 'SHARED' THEN site ELSE member END))
+             , BOOL_AND(share_full) -- only parent everybody has a value
+	  FROM volume_access
+            JOIN authorize_view ON party = parent
+        GROUP BY volume, child;
