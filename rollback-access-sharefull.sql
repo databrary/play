92c11172d07bbe35097386b22d19e44be1218062
kanishka <nish2575@gmail.com>
2017-12-19 10:58:49 -0500

    Modify volume_access_view to propagate value of share_full
    - share_full is only set for everybody group, remaining parties will
    receive the value if they are accessing through parent group everybody

--- rollback-access-sharefull.sql
+++ rollback-access-sharefull.sql
@@ -1,4 +1,14 @@
+
+DROP VIEW "volume_access_view";
+
+CREATE VIEW "volume_access_view" ("volume", "party", "access") AS
+	SELECT volume, party, individual FROM volume_access
+	UNION ALL
+	SELECT volume, child, MAX(LEAST(children, CASE WHEN children <= 'SHARED' THEN site ELSE member END))
+	  FROM volume_access JOIN authorize_view ON party = parent GROUP BY volume, child;
+
 -- remove check constraint?
 
 alter table audit."volume_access" drop column "share_full";
 alter table "volume_access" drop column "share_full";
+
