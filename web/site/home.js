3e1b934f01c304e90b08f4343fcd860b47a04cb2
myicicle <minyong.cho@databrary.org>
2018-01-25 16:21:56 -0500

    add login modal

--- web/site/home.js
+++ web/site/home.js
@@ -1,12 +1,33 @@
 'use strict';
 
 app.controller('site/home', [
-  '$scope', 'constantService', 'displayService', 'volume', 'tags', 'activity',
-  function ($scope, constants, display, volume, tags, activity) {
+  '$scope', 'constantService', 'displayService', 'volume', 'tags', 'activity', 'pageService',
+  function ($scope, constants, display, volume, tags, activity, page) {
     display.title = constants.message('welcome.title');
     $scope.volume = volume;
     $scope.tags = tags;
     $scope.activity = activity.activity;
     $scope.stats = activity.stats;
+    var modal = document.getElementById('loginModal');
+    var span = document.getElementsByClassName("close")[0];
+
+    $('.modal-button').click(function(e) {
+        e.preventDefault();
+        modal.style.display = "block";
+    });
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    if (page.models.Login.isLoggedIn())
+    	modal.style.display = "none";
   }
 ]);
