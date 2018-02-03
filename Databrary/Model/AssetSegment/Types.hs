88c18bf848c56497c2cd6a68438c2202f2e157e1
kanishka <nish2575@gmail.com>
2018-02-03 14:58:48 -0500

    Test cases for about half of the scenarios of assetSegmentRelease

--- Databrary/Model/AssetSegment/Types.hs
+++ Databrary/Model/AssetSegment/Types.hs
@@ -11,6 +11,8 @@ module Databrary.Model.AssetSegment.Types
   , Excerpt(..)
   , newExcerpt
   , excerptInSegment
+  -- for testing only
+  , testgetAssetSegmentRelease2
   ) where
 
 import Data.Foldable (fold)
@@ -98,62 +100,95 @@ instance Has (Id Format) AssetSegment where
   view = formatId . view
 
 -- TODO: move to unit test suite
-testmakeAssetSegment :: AssetSegment
-testmakeAssetSegment =
+testmakeAssetSegment :: AssetSlot -> Bool -> Maybe Release -> AssetSegment
+testmakeAssetSegment as hasExcerpt mRel =
   AssetSegment {
-      segmentAsset = testmakeAssetSlot
-    , assetSegment = Segment (Range.Empty)
-    , assetExcerpt = Nothing
+      segmentAsset = as
+    , assetSegment = fullSegment
+    , assetExcerpt = if hasExcerpt then Just (newExcerpt as fullSegment mRel) else Nothing
   }
   
-testmakeAssetSlot :: AssetSlot
-testmakeAssetSlot =
-  AssetSlot {
-      slotAsset = testmakeAsset
-    , assetSlot = Nothing -- Just testMakeSlot
-  }
+testmakeAssetSlot :: Id Volume -> Maybe Release -> Maybe Slot -> AssetSlot
+testmakeAssetSlot vid mRel ms =
+  let
+    asset = blankAsset (testmakeVolume vid)
+    asset2 = asset { assetRow = (assetRow asset) { assetRelease = mRel } }
+  in
+    AssetSlot { slotAsset = asset2 , assetSlot = ms }
 
-testmakeAsset :: Asset
-testmakeAsset = Asset
-  { assetRow = AssetRow
-    { assetId = error "blankAsset"
-    , assetFormat = unknownFormat
-    , assetRelease = Nothing
-    , assetName = Nothing
-    , assetDuration = Nothing
-    , assetSHA1 = Nothing
-    , assetSize = Nothing
+testmakeSlot :: Slot
+testmakeSlot =
+  Slot {
+      slotContainer =
+        Container {
+            containerRow =
+              ContainerRow { containerId = Id 200, containerTop = False, containerName = Nothing, containerDate = Nothing }
+          , containerRelease = Nothing
+          , containerVolume = realVolume
+          }
+    , slotSegment = fullSegment
     }
-  , assetVolume = testmakeVolume
-  }
 
-testmakeVolume :: Volume
-testmakeVolume =
+testmakeVolume :: Id Volume -> Volume
+testmakeVolume vid =
   blankVolume {
     volumeRow =
-      (volumeRow blankVolume) {
-          volumeId = Id 0
-        }
+      (volumeRow blankVolume) { volumeId = vid }
     }
+
+realVolume, databrarySystem :: Volume
+realVolume = testmakeVolume realVolumeId
+databrarySystem = testmakeVolume coreVolumeId
+realVolumeId :: Id Volume
+realVolumeId = Id 100
+
+containerAssetSlot :: Maybe Release -> AssetSlot
+containerAssetSlot mRel =
+  testmakeAssetSlot realVolumeId mRel (Just testmakeSlot)
+
+noExcerptContainerAssetNoRelease :: AssetSegment
+excerptNoReleaseContainerAssetNoRelease :: AssetSegment
+noExcerptContainerAssetNoRelease = testmakeAssetSegment (containerAssetSlot Nothing) False Nothing
+excerptNoReleaseContainerAssetNoRelease = testmakeAssetSegment (containerAssetSlot Nothing) True Nothing
+excerptNoReleaseContainerAssetHasRelease = testmakeAssetSegment (containerAssetSlot (Just ReleasePUBLIC)) True Nothing
+excerptPublicReleaseContainerAssetNoRelease = testmakeAssetSegment (containerAssetSlot Nothing) True (Just ReleasePUBLIC)
+excerptSharedReleaseContainerAssetNoRelease = testmakeAssetSegment (containerAssetSlot Nothing) True (Just ReleaseSHARED)
+excerptPrivateReleaseContainerAssetNoRelease = testmakeAssetSegment (containerAssetSlot Nothing) True (Just ReleasePRIVATE)
+
+assetReleaseForPartialAndFullShared, privateReleaseIgnoreSharing :: EffectiveRelease
+excerptReleaseForPartialAndFullShared :: EffectiveRelease
+excerptReleaseForFullySharedOnly :: EffectiveRelease
+assetReleaseForPartialAndFullShared = EffectiveRelease ReleasePUBLIC ReleasePUBLIC
+privateReleaseIgnoreSharing = EffectiveRelease ReleasePRIVATE ReleasePRIVATE
+excerptReleaseForPartialAndFullShared = EffectiveRelease ReleasePUBLIC ReleasePUBLIC
+excerptReleaseForPartialAndFullShared' = EffectiveRelease ReleaseSHARED ReleaseSHARED
+excerptReleaseForPartialAndFullShared'' = EffectiveRelease ReleasePRIVATE ReleasePRIVATE
+excerptReleaseForFullySharedOnly = EffectiveRelease ReleaseSHARED ReleasePRIVATE
 
 {- test cases: (focus on excerpt present or not test to begin with
 -- has excerpt
 --    excerpt rel, assetslot rel , pub vol res , priv vol res
---       x              x            priv          priv
---     pub              x            pub           pub
---     share            x            share         priv
---     priv             x            ....
---       x            has val        ------ see asset slot tests ---
---     pub            pub            pub           pub
+--       x              x            priv          priv         -- DONE
+--       x            has val (pub)  pub           pub     ------ see asset slot tests --- -- DONE
+--     pub              x            pub           pub  -- DONE
+--     share            x            share         priv??  -- DONE
+--     priv             x            priv          priv  -- DONE
+--     pub            pub            pub           pub 
+--     TODO: what other cases matter?
 -- no excerpt
---     see asset slot tests
+--     see asset slot tests -- DONE
 -}
+
 testgetAssetSegmentRelease2 :: [(EffectiveRelease, EffectiveRelease)]
 testgetAssetSegmentRelease2 =
-  [ (getAssetSegmentRelease2 (testmakeAssetSegment), EffectiveRelease ReleasePRIVATE ReleasePRIVATE)
+  [ (getAssetSegmentRelease2 noExcerptContainerAssetNoRelease, privateReleaseIgnoreSharing)
+  , (getAssetSegmentRelease2 excerptNoReleaseContainerAssetNoRelease, privateReleaseIgnoreSharing)
+  , (getAssetSegmentRelease2 excerptNoReleaseContainerAssetHasRelease, assetReleaseForPartialAndFullShared) -- wrong?
+  , (getAssetSegmentRelease2 excerptPublicReleaseContainerAssetNoRelease, excerptReleaseForPartialAndFullShared)
+  , (getAssetSegmentRelease2 excerptSharedReleaseContainerAssetNoRelease, excerptReleaseForPartialAndFullShared') -- wrong?
+  , (getAssetSegmentRelease2 excerptPrivateReleaseContainerAssetNoRelease, excerptReleaseForPartialAndFullShared'')
   ]
 -- END TODO
-
  
   -- when the assetslot has lower permissions than the excerpt, then use the excerpt's permissions
   -- when no excerpt is present, then assume no access
