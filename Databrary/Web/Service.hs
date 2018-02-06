344d203a51ed40abbb0a66b5f78c9a590d15e5dd
kanishka <nish2575@gmail.com>
2018-02-06 15:56:01 -0500

    ignore jscss version file; read version from file
    - the version used in js and css urls will now reference version specified in global file

--- Databrary/Web/Service.hs
+++ Databrary/Web/Service.hs
@@ -10,12 +10,24 @@ import Control.Concurrent.MVar (newMVar)
 #endif
 
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Encoding as TE
 import Databrary.Web.Types
 import Databrary.Web.Info
+import System.IO (FilePath, readFile)
+import Paths_databrary (getDataFileName)
+
+versionFile :: FilePath
+versionFile = "jsCssVersion.txt"
 
 initWeb :: IO Web
-initWeb =
-  fmap (\mp -> Web mp "1") $
+initWeb = do -- TODO: update postFixup to generate this, based on gz files
+  -- TODO: should this IO happen earlier?
+  versionFilePath <- getDataFileName versionFile
+  version <- (TE.encodeUtf8 . T.strip) <$> TIO.readFile versionFilePath -- strip - manually editing can introduce newline
+  fmap (\mp -> Web mp version) $
 #ifdef DEVEL
     newMVar =<<
 #endif
