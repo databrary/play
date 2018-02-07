5de8717324014d768388812862a073bf65fa5772
kanishka <nish2575@gmail.com>
2018-02-07 15:43:59 -0500

    Take mostly newer cabal2nix output, adding back custom phases
    - cracklib change to crack
    - more ffmpeg libraries were detected automatically

--- databrary.nix
+++ databrary.nix
@@ -1,48 +1,64 @@
 { mkDerivation, aeson, aeson-better-errors, array, attoparsec, base
-, bcrypt, blaze-html, blaze-markup, bytestring
-, case-insensitive, containers, cookie, cracklib, cryptonite
-, data-default-class, directory, fast-logger, ffmpeg
-, filepath, hashable, hjsonschema
-, http-client, http-client-tls, http-types, invertible
-, lifted-base, memory, mime-mail, mime-types, postgresql
-, monad-control, mtl, network, network-uri, parsec, posix-paths
-, postgresql-typed, process, range-set-list, regex-posix
-, resource-pool, resourcet, scientific, smtp-mail, stdenv
-, streaming-commons, template-haskell, text, th-lift
+, bcrypt, binary, blaze-html, blaze-markup, bytestring
+, case-insensitive, conduit-combinators, containers, cookie, crack
+, cryptonite, data-default-class, directory, fast-logger, ffmpeg
+, filepath, hashable, hjsonschema, http-client, http-client-tls
+, http-types, invertible, libavcodec, libavformat, libswscale
+, lifted-base, memory, mime-mail, mime-types, monad-control, mtl
+, network, network-uri, openssl, parsec, path, path-io, posix-paths
+, postgresql-simple, postgresql-typed, process, range-set-list
+, regex-posix, resource-pool, resourcet, scientific, smtp-mail
+, stdenv, streaming-commons, template-haskell, text, th-lift
 , th-lift-instances, time, transformers, transformers-base, unix
 , unordered-containers, utf8-string, vector, wai, wai-extra, warp
-, warp-tls, web-inv-route, xml, zlib, gargoyle, gargoyle-postgresql
-, postgresql-simple, zip, conduit-combinators, binary, path, path-io
+, warp-tls, web-inv-route, xml, zip, zlib
 , nodePackages, nodejs, openssl, dbName ? "databrary-nix-db", jdk
 , cpio, md5sum, cat, cut
 }:
-mkDerivation rec {
+mkDerivation {
   pname = "databrary";
   doCheck = false;
   version = "1";
   src = builtins.filterSource 
     (path: type: type == "directory" || baseNameOf path != ".git" || baseNameOf path == ".cabal" || path != dbName)
     ./.;
-  isLibrary = false;
+  isLibrary = true;
   isExecutable = true;
-  executableHaskellDepends = [
-    aeson aeson-better-errors array attoparsec base bcrypt
+  libraryHaskellDepends = [
+    aeson aeson-better-errors array attoparsec base bcrypt binary
     blaze-html blaze-markup bytestring case-insensitive
-    containers cookie cryptonite data-default-class directory
-    fast-logger filepath hashable
-    hjsonschema http-client http-client-tls http-types invertible
-    lifted-base memory mime-mail mime-types monad-control mtl network
-    network-uri parsec posix-paths postgresql-typed process range-set-list
-    regex-posix resource-pool resourcet scientific smtp-mail
-    streaming-commons template-haskell text th-lift th-lift-instances
-    time transformers transformers-base unix unordered-containers
-    utf8-string vector wai wai-extra warp warp-tls web-inv-route xml
-    zlib gargoyle gargoyle-postgresql zip conduit-combinators path path-io
-    postgresql-simple binary
+    conduit-combinators containers cookie cryptonite data-default-class
+    directory fast-logger filepath hashable hjsonschema http-client
+    http-client-tls http-types invertible lifted-base memory mime-mail
+    mime-types monad-control mtl network network-uri parsec path
+    path-io posix-paths postgresql-simple postgresql-typed process
+    range-set-list regex-posix resource-pool resourcet scientific
+    smtp-mail streaming-commons template-haskell text th-lift
+    th-lift-instances time transformers transformers-base unix
+    unordered-containers utf8-string vector wai wai-extra warp warp-tls
+    web-inv-route xml zip zlib
   ];
-  executableSystemDepends = [ cracklib openssl openssl.dev ];
+  librarySystemDepends = [ crack openssl openssl.dev ];
+  libraryPkgconfigDepends = [
+    ffmpeg libavcodec libavformat libswscale
+  ];
+  executableHaskellDepends = [
+    aeson aeson-better-errors array attoparsec base bcrypt binary
+    blaze-html blaze-markup bytestring case-insensitive
+    conduit-combinators containers cookie cryptonite data-default-class
+    directory fast-logger filepath hashable hjsonschema http-client
+    http-client-tls http-types invertible lifted-base memory mime-mail
+    mime-types monad-control mtl network network-uri parsec path
+    path-io posix-paths postgresql-simple postgresql-typed process
+    range-set-list regex-posix resource-pool resourcet scientific
+    smtp-mail streaming-commons template-haskell text th-lift
+    th-lift-instances time transformers transformers-base unix
+    unordered-containers utf8-string vector wai wai-extra warp warp-tls
+    web-inv-route xml zip zlib
+  ];
+  executableSystemDepends = [ crack openssl openssl.dev ];
   executablePkgconfigDepends = [
-    ffmpeg
+    ffmpeg libavcodec libavformat libswscale
   ];
   executableToolDepends = [
     postgresql.postgres
