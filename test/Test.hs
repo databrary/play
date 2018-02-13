fa6b149c9b8cda8ae6712aa90f69ad42eb8b4a80
Bryan Richter <bryan.richter@databrary.org>
2018-02-13 17:37:52 -0500

    Add sanity test with import

--- test/Test.hs
+++ test/Test.hs
@@ -1,9 +1,27 @@
 module Main (main) where
 
 import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.ExpectedFailure
+
+import Databrary.Main hiding (main)
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "databrary" []
+tests = testGroup "databrary" [testMain]
+
+testMain :: TestTree
+testMain = testGroup
+    "Databrary.Main"
+    [ expectFail $ testCase
+          "flagConfig sanity check"
+
+            ( do
+              (1 :: Int) @?= (2 :: Int) )
+          {-( do
+              flagConfig (FlagConfig "foo") @?= Left "foo"
+              flagConfig (FlagWeb) @?= Right FlagWeb
+          )-}
+    ]
