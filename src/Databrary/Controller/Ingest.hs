568c0d6be12b8ec6c5cae5a72e091dc97b767605
kanishka <nish2575@gmail.com>
2018-02-15 18:37:57 -0500

    Start writing received file. Attempt to recieve file, not done
    - receive csv file for participant upload

--- src/Databrary/Controller/Ingest.hs
+++ src/Databrary/Controller/Ingest.hs
@@ -10,6 +10,7 @@ import Control.Arrow (right)
 import Control.Monad (unless)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Attoparsec.ByteString as ATTO
+import qualified Data.ByteString as BS
 import qualified Data.Csv as CSV
 import qualified Data.Csv.Parser as CSVP
 import Data.Monoid ((<>))
@@ -58,10 +59,12 @@ postIngest = multipartAction $ action POST (pathId </< "ingest") $ \vi -> withAu
     abort ?!$> (,,)
       <$> ("run" .:> deform)
       <*> ("overwrite" .:> deform)
-      <*> ("json" .:>
-        (deformCheck "Must be JSON." (\f ->
-          fileContentType f `elem` ["text/json", "application/json"] || takeExtension (fileName f) == ".json")
-        =<< deform))
+      <*> ("json" .:> do
+              (fileInfo :: FileInfo JSON.Value) <- deform
+              (deformCheck
+                   "Must be JSON."
+                   (\f -> fileContentType f `elem` ["text/json", "application/json"] || takeExtension (fileName f) == ".json")
+                   fileInfo))
   r <- maybe
     (True <$ focusIO abortIngest)
     (\(r,o,j) -> runIngest $ right (map (unId . containerId . containerRow)) <$> ingestJSON v (fileContent j) r o)
@@ -69,6 +72,8 @@ postIngest = multipartAction $ action POST (pathId </< "ingest") $ \vi -> withAu
   unless r $ result $ response badRequest400 [] ("failed" :: String)
   peeks $ otherRouteResponse [] viewIngest (volumeId $ volumeRow v)
 
+-- maxCsvSize :: ?
+-- maxCsvSize = 3*1024*1024
 
 -- TODO: maybe put csv file save/retrieve in Databrary.Store module
 detectParticipantCSV :: ActionRoute (Id Volume)
@@ -76,8 +81,16 @@ detectParticipantCSV = action POST (pathJSON >/> pathId </< "detectParticipantCS
     -- checkMemberADMIN to start
     v <- getVolume PermissionEDIT vi
     reqCtxt <- peek
-    -- read file contents
-    let uploadFileContents = "idcol\nA1\nA2\n"
+    {-
+    csvFileInfo <- runFormFiles [("file", 10)] (Just $ htmlIngestForm v undefined) $ do -- TODO: don't use undefined
+      csrfForm -- ?
+      (fileInfo :: FileInfo BS.ByteString) <- "file" .:> deform  -- TODO: what is tempfile?
+      return fileInfo
+    -}
+    let uploadFileContents = "idcol\nA1\nA2\n" -- TODO: handle nothing
+        -- Just uploadFile = mCsvFile
+        -- uploadFileContents = fileContent uploadFile
+        uploadFileName = "yo.csv"
     let eCsvHeaders = ATTO.parseOnly (CSVP.csvWithHeader CSVP.defaultDecodeOptions) uploadFileContents
     case eCsvHeaders of
         Left err ->
@@ -85,7 +98,8 @@ detectParticipantCSV = action POST (pathJSON >/> pathId </< "detectParticipantCS
         Right (hdrs, _) -> do
             let mMpng = detectBestHeaderMapping (getHeaders hdrs)
             case mMpng of
-                Just mpng ->
+                Just mpng -> do
+                    liftIO (BS.writeFile ("/home/kanishka/tmp/" ++ uploadFileName) uploadFileContents)
                     pure
                         $ okResponse []
                             $ JSON.recordEncoding -- TODO: not record encoding
