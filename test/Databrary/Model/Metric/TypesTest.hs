b22b51e377b5a6f796af8d40e4a717e777e2486f
kanishka <nish2575@gmail.com>
2018-03-15 13:55:17 -0400

    Add missed new test module

---
+++ test/Databrary/Model/Metric/TypesTest.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+module Databrary.Model.Metric.TypesTest where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Databrary.Model.Metric.Types
+
+participantFieldMapping1 :: participantFieldMapping1
+participantFieldMapping1 =
+    ParticipantFieldMapping
+        { pfmId = Just "col1"
+        , pfmInfo = Nothing
+        , pfmDescription = Nothing
+        , pfmBirthdate = Nothing
+        , pfmGender = Just "col2"
+        , pfmRace = Nothing
+        , pfmEthnicity = Nothing
+        , pfmGestationalAge = Nothing
+        , pfmPregnancyTerm = Nothing
+        , pfmBirthWeight = Nothing
+        , pfmDisability = Nothing
+        , pfmLanguage = Nothing
+        , pfmCountry = Nothing
+        , pfmState = Nothing
+        , pfmSetting = Nothing
+        }
+
+tests :: TestTree
+tests = testGroup "Databrary.Model.Metric.Types"
+    [
+    ]
