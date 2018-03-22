c6199daf13b5f96878dce66da860d92a112dbd68
kanishka <nish2575@gmail.com>
2018-03-22 16:11:40 -0400

    Example site stats result for testing

--- test/Databrary/Model/Stats/TypesTest.hs
+++ test/Databrary/Model/Stats/TypesTest.hs
@@ -1,10 +1,28 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Databrary.Model.Stats.TypesTest where
 
+import qualified Data.Map as Map
+import qualified Data.Array.Unboxed as ArrayU
 import Test.Tasty
 import Test.Tasty.HUnit
 
 import Databrary.Model.Stats.Types
+import Databrary.Model.Category.Types
+import Databrary.Model.Id.Types
+import Databrary.Model.Permission.Types
+import Databrary.Model.Offset
+
+siteStats1 :: SiteStats
+siteStats1 =
+    SiteStats {
+          statsAuthorizedSite = ArrayU.listArray (minBound, maxBound) [0,1,2,3]
+        , statsVolumes = 2
+        , statsVolumesShared = 1
+        , statsAssets = 3
+        , statsAssetDuration = 0
+        , statsAssetBytes = 100
+        , statsRecords = Map.fromList []
+        }
 
 tests :: TestTree
 tests = testGroup "Databrary.Model.Stats.Types"
