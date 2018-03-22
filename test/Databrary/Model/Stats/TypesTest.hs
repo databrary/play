b69c19e83344dfa5087c709b3a749bd4fb750558
kanishka <nish2575@gmail.com>
2018-03-22 16:20:35 -0400

    Expand site stats example

--- test/Databrary/Model/Stats/TypesTest.hs
+++ test/Databrary/Model/Stats/TypesTest.hs
@@ -15,13 +15,22 @@ import Databrary.Model.Offset
 siteStats1 :: SiteStats
 siteStats1 =
     SiteStats {
-          statsAuthorizedSite = ArrayU.listArray (minBound, maxBound) [0,1,2,3]
+          statsAuthorizedSite = ArrayU.listArray (minBound, maxBound) [0,1,2,3,4,5]
         , statsVolumes = 2
         , statsVolumesShared = 1
         , statsAssets = 3
         , statsAssetDuration = 0
         , statsAssetBytes = 100
-        , statsRecords = Map.fromList []
+        , statsRecords =
+            Map.fromList
+              [ (Id 1, 1)
+              , (Id 2, 2)
+              , (Id 3, 3)
+              , (Id 4, 4)
+              , (Id 5, 5)
+              , (Id 6, 6)
+              , (Id 7, 7)
+              ] -- should be list of category to count of records
         }
 
 tests :: TestTree
