40f8acc0365d5976e53fef115ec1e7856cfae7ad
kanishka <nish2575@gmail.com>
2018-03-22 17:36:50 -0400

    Audit example for testing

--- test/Databrary/Model/Audit/TypesTest.hs
+++ test/Databrary/Model/Audit/TypesTest.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Databrary.Model.Audit.TypesTest where
 
+import Data.Time as Time
 import Database.PostgreSQL.Typed.Inet (PGInet(..))
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -13,6 +14,14 @@ auditIdentity1 =
     AuditIdentity {
           auditWho = Id 2
         , auditIp = PGInet 32 0
+        }
+
+audit1 :: Audit
+audit1 =
+    Audit {
+          auditWhen = Time.UTCTime (Time.fromGregorian 2000 1 2) (Time.secondsToDiffTime 0)
+        , auditIdentity = auditIdentity1
+        , auditAction = AuditActionOpen
         }
 
 tests :: TestTree
