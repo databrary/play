85a06e0362b9b6fda5d513db63775b8d6f99adac
kanishka <nish2575@gmail.com>
2018-03-22 17:05:16 -0400

    Remove unused

--- test/Databrary/Model/AuditTest.hs
+++ test/Databrary/Model/AuditTest.hs
@@ -5,21 +5,12 @@ import Control.Monad.Trans.Reader (ReaderT(..))
 -- import qualified Data.HashMap.Strict as HMP
 -- import qualified Data.Vector as V
 import Data.Functor.Identity
-import Database.PostgreSQL.Typed.Inet (PGInet(..), sockAddrPGInet)
+import Database.PostgreSQL.Typed.Inet (PGInet(..))
 import qualified Network.Wai as Wai
 import Test.Tasty
 import Test.Tasty.HUnit
 
--- import Databrary.Has
 import Databrary.Model.Audit
--- import Databrary.HTTP.Request
-
--- type MonadHas a c m = (Functor m, Applicative m, MonadReader c m, Has a c)
--- type MonadHasRequest c m = MonadHas Wai.Request Wai.Request Identity
--- ReaderT Wai.Request Identity a
-
--- withReaderT (req) :: ReaderT Wai.Request Identity a
--- runReaderT rdr req :: Identity a
 
 tests :: TestTree
 tests = testGroup "Databrary.Model.AuditTest"
