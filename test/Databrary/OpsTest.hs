dbe7e512fe2773d327a7bffed3d44e1a86ff5eb5
Bryan Richter <bryan.richter@databrary.org>
2018-04-18 10:50:48 -0400

    Placate warnings

--- test/Databrary/OpsTest.hs
+++ test/Databrary/OpsTest.hs
@@ -6,14 +6,16 @@ import Test.Tasty.HUnit
 import Databrary.Ops
 
 test_all :: TestTree
-test_all = testGroup "all"
-  [ testCase "rightJust-1"
-      (rightJust (Right 10) @?= Just 10)
-  , testCase "rightJust-2"
-      (rightJust (Left ()) @?= (Nothing :: Maybe Int))
-  , testCase "mergeBy-1"
-      (mergeBy compare [1,3] [2,4] @?= [1,2,3,4])
-  , testCase "groupTuplesBy-1"
-      (groupTuplesBy (==) [(True,1), (True,2), (False, 3)]
-         @?= [(True, [1,2]), (False, [3])])
-  ]
+test_all = testGroup
+    "all"
+    [ testCase "rightJust-1" (rightJust (Right 10) @?= (Just 10 :: Maybe Int))
+    , testCase "rightJust-2" (rightJust (Left ()) @?= (Nothing :: Maybe Int))
+    , testCase
+        "mergeBy-1"
+        (mergeBy compare [1, 3] [2, 4] @?= ([1, 2, 3, 4] :: [Int]))
+    , testCase
+        "groupTuplesBy-1"
+        (groupTuplesBy (==) [(True, 1), (True, 2), (False, 3)]
+            @?= ([(True, [1, 2]), (False, [3])] :: [(Bool, [Int])])
+        )
+    ]
