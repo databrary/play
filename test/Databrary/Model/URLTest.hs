0b6a118e3bace0e5c065ff0ad553f8ba3f2084c1
kanishka <nish2575@gmail.com>
2018-04-20 22:35:55 -0400

    validhdl, parseurl examples

--- test/Databrary/Model/URLTest.hs
+++ test/Databrary/Model/URLTest.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Databrary.Model.URLTest where
 
+import Network.URI
 import Test.Tasty
 import Test.Tasty.HUnit
 -- import Data.Time (fromGregorian, secondsToDiffTime)
@@ -9,4 +10,16 @@ import Databrary.Model.URL
 
 unit_validHDL_example :: Assertion
 unit_validHDL_example = do
+    validHDL "120.1/" @? "[0-9]+(.[0-9]+)+/.+ is valid"
+
+unit_validHDL_typical :: Assertion
+unit_validHDL_typical = do
     not (validHDL "a") @? "HDL should start with a digit"
+
+unit_validHDL_corner :: Assertion
+unit_validHDL_corner = do
+    validHDL "0.0.0/" @? "digits and dots ending with slash is okay"
+
+unit_parseURL_example :: Assertion
+unit_parseURL_example = do
+    parseURL "http://google.com" @?= parseURI "http://google.com"
