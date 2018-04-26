babf2f47b6c0ee22c38230acbc1d6f242e7b3572
kanishka <nish2575@gmail.com>
2018-04-26 11:15:14 -0400

    checkPassword example test, also test unset password

--- test/Databrary/Controller/LoginTest.hs
+++ test/Databrary/Controller/LoginTest.hs
@@ -8,10 +8,12 @@ import Databrary.Model.Party.Types
 -- import Databrary.Model.Party.TypesTest
 
 unit_checkPassword :: Assertion
-unit_checkPassword =
+unit_checkPassword = do
   -- example
-  checkPassword "pass1" (blankSiteAuth { accountPasswd = Just "pass1" }) @? "expected password passes"
+  checkPassword "pass1" (blankSiteAuth { accountPasswd = Just "$2b$12$w/ibpcJveMPnP1K0/OObcuggrFzwyzDsnTk1Vdr8Bk6EA9Gue1Bem" })
+    @? "expected password passes"
   -- typical
+  not (checkPassword "pass1" (blankSiteAuth { accountPasswd = Nothing })) @? "can't login when password not set yet"
   -- edge cases
 
 -- mkSiteAuth :: BS.ByteString -> SiteAuth
