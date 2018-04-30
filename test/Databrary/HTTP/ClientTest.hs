ef6eebecf6ff43df1e903288e0a8350cfd007da8
kanishka <nish2575@gmail.com>
2018-04-30 18:54:06 -0400

    Add example test httpRequestJSON

---
+++ test/Databrary/HTTP/ClientTest.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+module Databrary.HTTP.ClientTest where
+
+import qualified Network.HTTP.Client as HC
+import Test.Tasty
+import Test.Tasty.ExpectedFailure
+import Test.Tasty.HUnit
+
+import Data.Aeson
+import Data.Aeson.Types
+import Databrary.HTTP.Client
+
+test_httpRequestJSON :: TestTree
+test_httpRequestJSON =
+    -- don't need to run this every time, enable when desired
+    ignoreTest (testCase "httpRequestJSON" _unit_httpRequestJSON)
+
+_unit_httpRequestJSON :: Assertion
+_unit_httpRequestJSON = do
+    -- example
+    hc <- initHTTPClient
+    req <- HC.parseRequest "http://httpbin.org/get"
+    Just val <- httpRequestJSON req hc
+    let eVal = parseEither (withObject "req" (\o -> o .: "url")) val
+    eVal @?= (Right "http://httpbin.org/get" :: Either String String)
