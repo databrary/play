aa97105ebdf350d4e55f6ad9d7b89f5177ce371f
kanishka <nish2575@gmail.com>
2018-04-30 18:30:51 -0400

    Action.Request.isDatabraryClient example

---
+++ test/Databrary/Action/RequestTest.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+module Databrary.Action.RequestTest where
+
+import Network.Wai
+import Network.Wai.Internal
+import Test.Tasty.HUnit
+
+import Databrary.Action.Request
+
+unit_isDatabraryClient :: Assertion
+unit_isDatabraryClient = do
+    -- example
+    isDatabraryClient defaultRequest @?= False
+    -- typical
+    isDatabraryClient (defaultRequest { requestHeaders = [("x-requested-with", "DatabraryClient")] }) @?= True
