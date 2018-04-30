fb8a392cf1a22bb722a2754ab0de9ffd3ef7b068
kanishka <nish2575@gmail.com>
2018-04-30 18:17:42 -0400

    Files.hashFile, unRawFilePath, compareFiles examples

--- test/Databrary/FilesTest.hs
+++ test/Databrary/FilesTest.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Databrary.FilesTest where
 
+import Crypto.Hash (SHA1, Digest)
+-- import System.IO
 import System.Posix.Files.ByteString
 import Test.Tasty.HUnit
 
@@ -10,6 +13,13 @@ unit_rawFilePath = do
     -- example
     res <- rawFilePath "/a/b"
     res @?= "/a/b"
+
+unit_unRawFilePath :: Assertion
+unit_unRawFilePath = do
+    -- example
+    res <- rawFilePath "/a/b"
+    unres <- unRawFilePath res
+    unres @?= "/a/b"
 
 unit_removeFile :: Assertion
 unit_removeFile = do
@@ -23,3 +33,18 @@ unit_createDir = do
     created1 <- createDir "/tmp/testcreate" ownerWriteMode
     created2 <- createDir "/tmp/testcreate" ownerWriteMode
     not created1 || (created1 && not created2) @? "Can't create same directory twice"
+
+unit_compareFiles :: Assertion
+unit_compareFiles = do
+    -- example
+    writeFile "/tmp/comparefilestest" "abc"
+    writeFile "/tmp/comparefilestest2" "abc"
+    res <- compareFiles "/tmp/comparefilestest" "/tmp/comparefilestest2"
+    res @? "same contents should be same"
+
+unit_hashFiles :: Assertion
+unit_hashFiles = do
+    -- example
+    writeFile "/tmp/hashFilesTest" "efg"
+    (dig :: Digest SHA1) <- hashFile "/tmp/hashFilesTest"
+    show dig @?= "cbf019b764b9477080c5a9a748a2911a5fa6d614"
