5ad05a30d68c542e73039ff64d454bd641807196
kanishka <nish2575@gmail.com>
2018-05-02 19:09:25 -0400

    outline volume metric initial tests

---
+++ test/Databrary/Model/VolumeMetricTest.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+module Databrary.Model.VolumeMetricTest where
+
+import Test.Tasty.HUnit
+
+-- import Databrary.Model.Id.Types
+-- import Databrary.Model.Volume
+-- import Databrary.Model.Category
+-- import Databrary.Model.Metric
+-- import Databrary.Model.VolumeMetric
+-- import TestHarness
+
+-- unit_assetBacked :: Assertion
+-- unit_assetBacked =
+  -- example
+  -- typical
+  -- edge cases
+
+-- lookup on vol 1 == []
+-- lookup part on vol 1 == []
+-- add vol cat on vol 1, add vol metric on vol 1, lookup vol 1 == [added]
+--  " w/ part cat and metric
+-- add category, add metric, remove metric, lookup vol 1 == []
