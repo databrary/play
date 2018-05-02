0e8c009339949262547a13fe621e58dd636b8fe2
kanishka <nish2575@gmail.com>
2018-05-02 18:16:50 -0400

    Document, cleanup examples for URL module

--- src/Databrary/Model/URL.hs
+++ src/Databrary/Model/URL.hs
@@ -20,30 +20,39 @@ import qualified Text.Blaze as H
 
 import qualified Databrary.Store.Config as C
 
+-- | Prepare a URI value for using in a query or storing in a table
 toPG :: URI -> String
 toPG u = uriToString id u ""
 
+-- | Extract a URI value, after it has been retrieved using a query
 fromPG :: String -> URI
 fromPG u = fromMaybe (error $ "pgDecode URI: " ++ u) $ parseURI u
 
+-- | From URI value into value to be provided to database
 instance PGParameter "text" URI where
   pgEncode t = pgEncode t . toPG
   pgEncodeValue e t = pgEncodeValue e t . toPG
   pgLiteral t = pgLiteral t . toPG
+-- | From database value to URI value
 instance PGColumn "text" URI where
   pgDecode t = fromPG . pgDecode t
   pgDecodeValue e t = fromPG . pgDecodeValue e t
 
+-- | Format a URL value for inclusion in a JSON object
 instance ToJSON URI where
   toJSON = toJSON . show
 
+-- | Extract a URI value from a configuration entry
 instance C.Configurable URI where
   config = parseAbsoluteURI <=< C.config
 
+-- | Format a URI for display in a server side generated html page
 instance H.ToValue URI where
   toValue = H.stringValue . show . urlLink
   preEscapedToValue = H.preEscapedStringValue . show . urlLink
 
+-- | A valid HDL handle consists of digits with periods interleaved, ending with a slash (following by anything).
+-- See handle.net for more information. What is generating and using HDL urls?
 validHDL :: String -> Bool
 validHDL = v0 (0 :: Int) where
   v0 n (c:s) | isDigit c = v1 n s
@@ -52,15 +61,19 @@ validHDL = v0 (0 :: Int) where
   v1 n ('.':s) = v0 (succ n) s
   v1 n s = v0 n s
 
+-- | Build an HDL url from a DOI
 hdlURL :: String -> URI
 hdlURL doi = URI "hdl:" Nothing doi "" ""
 
+-- | Start from either a shorthand DOI value or a doi/hdl scheme or doi domain, and
+-- expand out to canonical HDL based URI. For all other http/https URLs, pass value through
 parseURL :: String -> Maybe URI
 parseURL d@('1':'0':'.':c:_) | isDigit c = parseURL $ "doi:" ++ d
 parseURL s = do
   u <- parseURI s
   if uriScheme u `elem` ["doi:","hdl:"] && isNothing (uriAuthority u) ||
-     uriScheme u == "http:" && (uriAuthority u == Just (URIAuth "" "dx.doi.org" "") || uriAuthority u == Just (URIAuth "" "doi.org" ""))
+     uriScheme u == "http:"
+       && (uriAuthority u == Just (URIAuth "" "dx.doi.org" "") || uriAuthority u == Just (URIAuth "" "doi.org" ""))
     then do
       let p = dropWhile ('/' ==) $ uriPath u
       guard $ validHDL p
@@ -73,9 +86,12 @@ parseURL s = do
       guard $ uriScheme u `elem` ["http:","https:"]
       return u
 
+-- | Utility for building a URI value from a domain and path
 httpAuth :: String -> URI -> URI
 httpAuth a u = u{ uriScheme = "http:", uriAuthority = Just (URIAuth "" a ""), uriPath = '/':uriPath u }
 
+-- | Expand special doi and hdl scheme URIs to equivalent http scheme URIs.
+-- Allow http URIs to pass through
 urlLink :: URI -> URI
 urlLink u@URI{ uriScheme = "hdl:" } = httpAuth "hdl.handle.net" u
 urlLink u@URI{ uriScheme = "doi:" } = httpAuth "doi.org" u
--- test/Databrary/Model/URLTest.hs
+++ test/Databrary/Model/URLTest.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Databrary.Model.URLTest where
 
-import Data.Maybe
+import Data.Monoid ((<>))
+import Hedgehog
+import Hedgehog.Gen as Gen
+import Hedgehog.Range as Range
 import Network.URI
 import Test.Tasty.HUnit
 
@@ -16,12 +19,37 @@ unit_validHDL = do
     -- edge case
     validHDL "0.0.0/" @? "digits and dots ending with slash is okay"
 
+-- hdlURL
+
 unit_parseURL :: Assertion
 unit_parseURL = do
     -- example
     parseURL "http://google.com" @?= parseURI "http://google.com"
+    -- typical
+    parseURL "10.1000/xyz123" @?= parseURI "hdl:10.1000/xyz123"
+    parseURL "http://doi.org/10.1000/xyz123" @?= parseURI "hdl:10.1000/xyz123"
 
 unit_urlLink :: Assertion
 unit_urlLink = do
     -- example
-    (urlLink . fromJust . parseURI) "hdl:rest" @?= (fromJust . parseURI) "http://hdl.handle.net/rest"
+    (fmap urlLink . parseURI) "hdl:rest" @?= parseURI "http://hdl.handle.net/rest"
+
+-- gen doi value
+
+-- gen hdl value
+
+-- gen doi url
+
+-- gen hdl url
+
+genGeneralURI :: Gen URI
+genGeneralURI = do
+    domain <- Gen.string (Range.constant 1 20) Gen.alphaNum
+    pathSeg1 <- Gen.string (Range.constant 1 20) Gen.alphaNum -- TODO: generate multiple segs, allowed chars?
+    scheme1 <- Gen.element ["http", "https"]
+    pure
+        (nullURI {
+              uriScheme = scheme1
+            , uriAuthority = Just (URIAuth "" ("www." <> domain <> ".com") "") -- TODO: choose on prefix and suffix?
+            , uriPath = "/" <> pathSeg1
+            })
