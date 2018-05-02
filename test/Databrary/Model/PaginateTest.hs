7bca542bb61196f64c31f98f5fec9458d090289d
kanishka <nish2575@gmail.com>
2018-05-02 18:58:54 -0400

    Examples and docs for Paginate

--- test/Databrary/Model/PaginateTest.hs
+++ test/Databrary/Model/PaginateTest.hs
@@ -1,11 +1,32 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Databrary.Model.PaginateTest where
 
--- import Test.Tasty
+import Hedgehog
+import Hedgehog.Gen as Gen
+-- import Hedgehog.Range as Range
 import Test.Tasty.HUnit
 
 import Databrary.Model.Paginate
 
-unit_paginate_succ_example :: Assertion
-unit_paginate_succ_example = do
+unit_paginate_succ :: Assertion
+unit_paginate_succ = do
+    -- example
     succ (Paginate 1 20) @?= Paginate 21 20
+
+unit_paginate_pred :: Assertion
+unit_paginate_pred = do
+    -- example
+    pred (Paginate 100 25) @?= Paginate 75 25
+
+unit_paginate_toEnum :: Assertion
+unit_paginate_toEnum = do
+    -- example
+    toEnum 3 @?= Paginate 75 25
+
+unit_paginate_fromEnum :: Assertion
+unit_paginate_fromEnum = do
+    -- example
+    fromEnum (Paginate 75 25) @?= 100
+
+genPaginate :: Gen Paginate
+genPaginate = Gen.enumBounded
