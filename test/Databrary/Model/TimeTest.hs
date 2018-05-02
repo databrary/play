35c36f9cd752043bc529a10c93b0161ab6cdc386
kanishka <nish2575@gmail.com>
2018-05-02 16:54:52 -0400

    Document, cleanup examples for Time module

--- test/Databrary/Model/TimeTest.hs
+++ test/Databrary/Model/TimeTest.hs
@@ -1,16 +1,53 @@
 module Databrary.Model.TimeTest
 where
 
-import Test.Tasty
+import Data.Aeson
+import Data.Time
+import Hedgehog
+import Hedgehog.Gen as Gen
+import Hedgehog.Range as Range
+-- import Test.Tasty
 import Test.Tasty.HUnit
 
-import Data.Time (fromGregorian)
 import Databrary.Model.Time
 
-test_all :: [TestTree]
-test_all =
-  [ testCase "dateYear-1"
-      (dateYear (fromGregorian 2017 1 2) @?= 2017)
-  , testCase "maskDateIf-1"
-      (maskedYear (maskDateIf True (fromGregorian 2017 1 2)) @?= 2017)
-  ]
+unit_dateYear :: Assertion
+unit_dateYear =
+    -- example
+    dateYear (fromGregorian 2017 1 2) @?= 2017
+
+unit_maskDateIf :: Assertion
+unit_maskDateIf = do
+   -- example
+   maskedYear (maskDateIf True (fromGregorian 2017 1 2)) @?= 2017
+
+-- potential properties:
+-- formattime on a masked date will not show month or day
+-- formatTime on unmasked date will show everything
+-- all dates will show at least the year
+
+unit_formatTime_MaskedDate :: Assertion
+unit_formatTime_MaskedDate = do
+    -- example
+    formatTime defaultTimeLocale "%F" (maskDateIf True (fromGregorian 2017 1 2)) @?= "2017-XX-XX"
+
+unit_toJSON_MaskedDate :: Assertion
+unit_toJSON_MaskedDate = do
+    -- example
+    encode (maskDateIf True (fromGregorian 2017 1 2)) @?= "2017"
+    -- typical
+    encode (maskDateIf False (fromGregorian 2017 1 2)) @?= "\"2017-01-02\""
+
+genDate :: Gen Date
+genDate =
+      fromGregorian
+          <$> Gen.integral (Range.constant 1990 2015)
+          <*> Gen.integral (Range.constant 1 12)
+          <*> Gen.integral (Range.constant 1 28)
+
+genMaskedDate :: Gen MaskedDate
+genMaskedDate = do
+    dt <- genDate
+    mask <- Gen.bool
+    pure (maskDateIf mask dt)
+
