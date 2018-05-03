f2f659a0d700f3f5efb26edbf1a0cd5dc4b6e937
kanishka <nish2575@gmail.com>
2018-05-03 01:51:22 -0400

    Offset docs and examples

--- test/Databrary/Model/OffsetTest.hs
+++ test/Databrary/Model/OffsetTest.hs
@@ -1,12 +1,41 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Databrary.Model.OffsetTest where
 
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Fixed
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 -- import Test.Tasty
 import Test.Tasty.HUnit
 
 import Databrary.Model.Offset
 
-unit_readOffset_example :: Assertion
-unit_readOffset_example = do
+-- offsetMillis :
+
+-- diffTimeOffset
+
+-- offsetDiffTime
+
+-- Show
+-- Read
+unit_readOffset :: Assertion
+unit_readOffset = do
+    -- example
     let offset = "11:22:33.0"
     (show . (read :: String -> Offset)) offset @?= offset
+
+unit_toJSON_Offset :: Assertion
+unit_toJSON_Offset =
+    -- example
+    encode (Offset 90.123) @?= "90123"
+
+unit_parseJSON_Offset :: Assertion
+unit_parseJSON_Offset =
+    -- example
+    parseEither parseJSON (Number 1234) @?= (Right . Offset) 1.234
+
+genOffset :: Milli -> Gen Offset
+genOffset totalLength =
+    Offset <$> Gen.realFrac_ (Range.constant 0 totalLength)
