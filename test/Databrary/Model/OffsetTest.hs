9db1c738c2d9d7c2681912b0b0906ff3177ff4f4
kanishka <nish2575@gmail.com>
2018-05-03 02:10:54 -0400

    Offset document, examples

--- test/Databrary/Model/OffsetTest.hs
+++ test/Databrary/Model/OffsetTest.hs
@@ -12,29 +12,20 @@ import Test.Tasty.HUnit
 
 import Databrary.Model.Offset
 
--- offsetMillis :
+unit_Offset_examples :: Assertion
+unit_Offset_examples = do
+    diffTimeOffset 12.345678 @?= Offset 12.345
+    offsetDiffTime (Offset 12.345) @?= 12.345
+    offsetMillis (Offset 1.234) @?= 1234
+    encode (Offset 90.123) @?= "90123"
+    parseEither parseJSON (Number 1234) @?= (Right . Offset) 1.234
 
--- diffTimeOffset
+-- TOOD: property difftimeoffset . offsetDifftime o === o
 
--- offsetDiffTime
-
--- Show
--- Read
-unit_readOffset :: Assertion
-unit_readOffset = do
-    -- example
+unit_Offset_readshow_inverts :: Assertion
+unit_Offset_readshow_inverts = do
     let offset = "11:22:33.0"
     (show . (read :: String -> Offset)) offset @?= offset
-
-unit_toJSON_Offset :: Assertion
-unit_toJSON_Offset =
-    -- example
-    encode (Offset 90.123) @?= "90123"
-
-unit_parseJSON_Offset :: Assertion
-unit_parseJSON_Offset =
-    -- example
-    parseEither parseJSON (Number 1234) @?= (Right . Offset) 1.234
 
 genOffset :: Milli -> Gen Offset
 genOffset totalLength =
