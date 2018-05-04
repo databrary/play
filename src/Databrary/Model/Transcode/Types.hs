b7ad5f156c79bf93771286eb82fad4a0b019348e
kanishka <nish2575@gmail.com>
2018-05-03 22:42:26 -0400

    Remove unused Transcode.SQL
    - contents were expanded in Transcode a while back

--- src/Databrary/Model/Transcode/Types.hs
+++ src/Databrary/Model/Transcode/Types.hs
@@ -6,9 +6,14 @@ module Databrary.Model.Transcode.Types
   , transcodeAsset
   , transcodeOrig
   , transcodeId
+  -- TODO: don't re-export from Transcode
+  , makeTranscodeRow
+  , makeTranscode
+  , makeOrigTranscode
   ) where
 
 import qualified Data.ByteString as BS
+import Data.Maybe (fromMaybe)
 
 import Databrary.Model.Kind
 import Databrary.Model.Id.Types
@@ -17,6 +22,8 @@ import Databrary.Model.Segment
 import Databrary.Model.Asset.Types
 import Databrary.Model.AssetRevision.Types
 import Databrary.Model.Party.Types
+import Databrary.Model.Permission.Types
+import Databrary.Model.Volume.Types
 
 type TranscodePID = Int32
 type TranscodeArgs = [String]
@@ -45,3 +52,12 @@ transcodeId = Id . unId . assetId . assetRow . transcodeAsset
 instance Kinded Transcode where
   kindOf _ = "transcode"
 
+makeTranscodeRow :: Segment -> [Maybe String] -> Maybe Timestamp -> Maybe Int32 -> Maybe BS.ByteString -> SiteAuth -> AssetRevision -> Transcode
+makeTranscodeRow s f t p l u a =
+  Transcode a u s (map (fromMaybe (error "NULL transcode options")) f) t p l
+
+makeOrigTranscode :: (AssetRevision -> Transcode) -> AssetRow -> Asset -> Transcode
+makeOrigTranscode f a o = f $ AssetRevision (Asset a $ assetVolume o) o
+
+makeTranscode :: (Asset -> Transcode) -> AssetRow -> (Permission -> VolumeAccessPolicy -> Volume) -> Transcode
+makeTranscode t o vp = t $ Asset o $ vp PermissionADMIN PermLevelDefault
