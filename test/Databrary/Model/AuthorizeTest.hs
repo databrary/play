76dbb1b1fbfb2ba0f0d326a3d1d1f8377094c0bf
kanishka <nish2575@gmail.com>
2018-05-10 19:28:56 -0400

    Start exercising lookupAuthorize, which is part of postAuthorize

--- test/Databrary/Model/AuthorizeTest.hs
+++ test/Databrary/Model/AuthorizeTest.hs
@@ -1,15 +1,27 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Databrary.Model.AuthorizeTest where
 
+import Test.Tasty
 import Test.Tasty.HUnit
 
 import Databrary.Model.Authorize
 import Databrary.Model.Party
+import Databrary.Model.Permission
+import TestHarness
 
 -- session exercise various logic in Authorize
-unit_Authorize_examples :: Assertion
-unit_Authorize_examples = do
+test_Authorize_examples :: TestTree
+test_Authorize_examples = testCaseSteps "Authorize examples" $ \step -> do
   (authorizeExpires . selfAuthorize) nobodyParty @?= Nothing
+
+  cn <- connectTestDb
+  step "Given an admin user"
+  let adminUser = nobodyParty { partyRow = (partyRow nobodyParty) { partyId = Id 7 } }
+  step "When we look at its direct authorization on databrary site"
+  Just auth <- runReaderT (lookupAuthorize adminUser rootParty) TestContext { ctxConn = cn }
+  step "Then we expect the authorization to have site and member level of ADMIN"
+  (authorizeAccess . authorization) auth @?= Access { accessSite' = PermissionADMIN, accessMember' = PermissionADMIN }
+
   -- create super user account (identity not needed)
   -- save authorize as ADMIN on databrary (identity not needed)
   -- lookup super user site auth
