7da1993edc70e1fb34e55d739436cc894926946b
kanishka <nish2575@gmail.com>
2018-05-10 18:40:28 -0400

    Simple examples for Solr.Document

---
+++ test/Databrary/Solr/DocumentTest.hs
@@ -0,0 +1,20 @@
+module Databrary.Solr.DocumentTest where
+
+import Data.Aeson
+-- import Data.Text
+import Test.Tasty.HUnit
+
+import Databrary.Model.Id
+import Databrary.Model.Metric
+import Databrary.Model.Segment
+import Databrary.Model.Tag
+import Databrary.Solr.Document
+
+-- Session demonstrating how to use Document functions
+unit_Document_examples :: Assertion
+unit_Document_examples = do
+    metricField participantMetricBirthWeight @?= "record_numeric_birth_weight"
+    toJSON (SolrRecordMeasures [(participantMetricId, "1")]) @?= object [("record_text_ID",String "1")]
+    toJSON (SolrSegment emptySegment) @?= String "empty"
+    toJSON (SolrTagId "1s" (Id 1) (TagName "tagone")) @?=
+        object [("tag_name",String "tagone"),("tag_id",Number 1.0),("content_type",String "tag_id"),("id",String "1s")]
