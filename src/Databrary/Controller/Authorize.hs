7718f1b423a984ae29fd7ff49a09565552a06a68
kanishka <nish2575@gmail.com>
2018-05-12 00:53:24 -0400

    Document authorize

--- src/Databrary/Controller/Authorize.hs
+++ src/Databrary/Controller/Authorize.hs
@@ -73,30 +73,30 @@ removeAuthorizeNotify priorAuth =
     let noReplacementAuthorization = Nothing
     in updateAuthorize priorAuth noReplacementAuthorization
 
--- | Remove (when only first argument provided) or swap in new authorization, triggering notifications.
+-- | Remove (when only first argument provided) or create/swap in new version of authorization, triggering notifications.
 -- Do nothing when neither an old or new auth has been provided.
 updateAuthorize :: Maybe Authorize -> Maybe Authorize -> Handler ()
-updateAuthorize priorAuth newAuth
-  | Just auth <- authorization <$> (priorAuth <|> newAuth :: Maybe Authorize) = do
+updateAuthorize priorAuth newOrUpdatedAuth
+  | Just priorElseNewCore <- authorization <$> (priorAuth <|> newOrUpdatedAuth :: Maybe Authorize) = do
   maybe
     (mapM_ removeAuthorize priorAuth)
     changeAuthorize
-    newAuth
-  when (on (/=) (foldMap $ authorizeAccess . authorization) newAuth priorAuth) $ do
-    let perm = accessSite <$> newAuth
-    dl <- partyDelegates $ authorizeParent auth
+    newOrUpdatedAuth
+  when (on (/=) (foldMap $ authorizeAccess . authorization) newOrUpdatedAuth priorAuth) $ do
+    let perm = accessSite <$> newOrUpdatedAuth
+    dl <- partyDelegates $ authorizeParent priorElseNewCore
     forM_ dl $ \t ->
       createNotification (blankNotification t NoticeAuthorizeChildGranted)
-        { notificationParty = Just $ partyRow $ authorizeChild auth
+        { notificationParty = Just $ partyRow $ authorizeChild priorElseNewCore
         , notificationPermission = perm
         }
-    forM_ (partyAccount $ authorizeChild auth) $ \t ->
+    forM_ (partyAccount $ authorizeChild priorElseNewCore) $ \t ->
       createNotification (blankNotification t NoticeAuthorizeGranted)
-        { notificationParty = Just $ partyRow $ authorizeParent auth
+        { notificationParty = Just $ partyRow $ authorizeParent priorElseNewCore
         , notificationPermission = perm
         }
   updateAuthorizeNotifications priorAuth
-      $ fromMaybe (Authorize auth{ authorizeAccess = mempty } Nothing) newAuth
+      $ fromMaybe (Authorize priorElseNewCore{ authorizeAccess = mempty } Nothing) newOrUpdatedAuth
 updateAuthorize ~Nothing ~Nothing = return ()
 
 createAuthorize :: (MonadAudit c m) => Authorize -> m ()
@@ -131,9 +131,11 @@ postAuthorize = action POST (pathAPI </>> pathPartyTarget </> pathAuthorizeTarge
       now <- peek
       let maxexp = addGregorianYearsRollOver 2 $ utctDay now -- TODO: use timestamp from actioncontext instead of now?
           minexp = fromGregorian 2000 1 1
+      -- the new version of this authorization (possibly first) should either be ...
       a <- runForm ((api == HTML) `thenUse` (htmlAuthorizeForm c')) $ do
         csrfForm
         delete <- "delete" .:> deform
+        -- 1. Nothing (causing deletion if there was a request or old auth)
         delete `unlessReturn` (do
           site <- "site" .:> deform
           member <- "member" .:> deform
@@ -141,10 +143,13 @@ postAuthorize = action POST (pathAPI </>> pathPartyTarget </> pathAuthorizeTarge
             "expires" .:>
               (deformCheck "Expiration must be within two years." (all (\e -> su || e > minexp && e <= maxexp))
                =<< (<|> (su `unlessUse` maxexp)) <$> deformNonEmpty deform)
+          -- 2. A Just with the new or updated approved authorization
           return $ makeAuthorize (Access site member) (fmap with1210Utc expires) child parent)
+      -- Perform the indicated change decided above in the value of "a"
       updateAuthorize c a
       return a
   case api of
+    -- respond with a copy of the updated authorization, if any
     JSON -> return $ okResponse [] $ JSON.pairs $ foldMap authorizeJSON resultingAuthorize <> "party" JSON..=: partyJSON o
     HTML -> peeks $ otherRouteResponse [] viewAuthorize arg
 
