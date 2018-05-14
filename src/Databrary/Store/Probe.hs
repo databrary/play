ae3ca88e9b6d45789d44f0944010b50bc6f9c354
kanishka <nish2575@gmail.com>
2018-05-14 00:26:01 -0400

    Example usage of Store.Probe functions; decouple probefile from full web handler
    - probeFile doesn't need all the context of a web handler

--- src/Databrary/Store/Probe.hs
+++ src/Databrary/Store/Probe.hs
@@ -9,6 +9,7 @@ module Databrary.Store.Probe
 
 import Control.Arrow (left)
 import Control.Exception (try)
+import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.Trans.Except (ExceptT(..), runExceptT, throwE)
 import qualified Data.ByteString as BS
 import Data.List (isPrefixOf)
@@ -27,7 +28,7 @@ import Databrary.Model.Offset
 import Databrary.Model.Container.Types
 import Databrary.Model.AssetSlot
 import Databrary.Store.AV
-import Databrary.Action.Types
+-- import Databrary.Action.Types
 
 data Probe
   = ProbePlain
@@ -42,7 +43,7 @@ probeLength :: Probe -> Maybe Offset
 probeLength ProbeAV{ probeAV = av } = avProbeLength av
 probeLength _ = Nothing
 
-probeFile :: BS.ByteString -> RawFilePath -> Handler (Either T.Text Probe)
+probeFile :: (MonadIO m, MonadHas AV c m) => BS.ByteString -> RawFilePath -> m (Either T.Text Probe)
 probeFile n f = runExceptT $ maybe
   (throwE $ "unknown or unsupported format: " <> TE.decodeLatin1 (takeExtension n))
   (\fmt -> case formatTranscodable fmt of
@@ -59,6 +60,7 @@ probeFile n f = runExceptT $ maybe
       | otherwise -> fail "unhandled format conversion")
   $ getFormatByFilename n
 
+-- TODO: make this pure
 probeAutoPosition :: MonadDB c m => Container -> Maybe Probe -> m Offset
 probeAutoPosition Container{ containerRow = ContainerRow { containerDate = Just d } } (Just ProbeAV{ probeAV = AVProbe{ avProbeDate = Just (ZonedTime (LocalTime d' t) _) } })
   | dd >= -1 && dd <= 1 && dt >= negate day2 && dt <= 3*day2 = return $ diffTimeOffset dt where
