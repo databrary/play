bd2fa929bc9759d3075d081dc8a15f0bbb7f0c23
Bryan Richter <bryan.richter@databrary.org>
2018-06-15 13:15:54 -0400

    Clean up after file creation tests

--- test/Databrary/FilesTest.hs
+++ test/Databrary/FilesTest.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Databrary.FilesTest where
 
+import Control.Exception (finally)
 import Crypto.Hash (SHA1, Digest)
--- import System.IO
-import System.Posix.Files.ByteString
+import Data.ByteString.Char8 (unpack)
+import System.Posix.ByteString (ownerWriteMode, removeDirectory)
 import Test.Tasty.HUnit
 
 import Databrary.Files
@@ -28,23 +30,30 @@ unit_removeFile = do
     removed @?= False
 
 unit_createDir :: Assertion
-unit_createDir = do
-    -- example
-    created1 <- createDir "/tmp/testcreate" ownerWriteMode
-    created2 <- createDir "/tmp/testcreate" ownerWriteMode
-    not created1 || (created1 && not created2) @? "Can't create same directory twice"
+unit_createDir =
+    finally
+        (do -- example
+            created1 <- createDir "/tmp/testcreate" ownerWriteMode
+            created2 <- createDir "/tmp/testcreate" ownerWriteMode
+            not created1 || (created1 && not created2) @? "Can't create same directory twice")
+        (removeDirectory "/tmp/testcreate")
 
 unit_compareFiles :: Assertion
-unit_compareFiles = do
-    -- example
-    writeFile "/tmp/comparefilestest" "abc"
-    writeFile "/tmp/comparefilestest2" "abc"
-    res <- compareFiles "/tmp/comparefilestest" "/tmp/comparefilestest2"
-    res @? "same contents should be same"
+unit_compareFiles =
+    let file1 = "/tmp/comparefilestest"
+        file2 = "/tmp/comparefilestest2"
+    in finally
+        (do -- example
+            writeFile (unpack file1) "abc"
+            writeFile (unpack file2) "abc"
+            res <- compareFiles file1 file2
+            res @? "same contents should be same")
+        (mapM_ removeFile [file1, file2])
 
 unit_hashFiles :: Assertion
-unit_hashFiles = do
-    -- example
-    writeFile "/tmp/hashFilesTest" "efg"
-    (dig :: Digest SHA1) <- hashFile "/tmp/hashFilesTest"
-    show dig @?= "cbf019b764b9477080c5a9a748a2911a5fa6d614"
+unit_hashFiles = finally
+    (do -- example
+        writeFile "/tmp/hashFilesTest" "efg"
+        (dig :: Digest SHA1) <- hashFile "/tmp/hashFilesTest"
+        show dig @?= "cbf019b764b9477080c5a9a748a2911a5fa6d614")
+    (removeFile "/tmp/hashFilesTest")
