145a52d27063626a01f4d1f8416d7fb4e2f9e316
kanishka <nish2575@gmail.com>
2018-06-25 16:24:53 -0400

    Remove commented out code

--- src/Model/VolumeAccess/Types.hs
+++ src/Model/VolumeAccess/Types.hs
@@ -6,7 +6,6 @@ module Model.VolumeAccess.Types
 
 import Data.Int (Int16)
 
--- import Has (Has(..))
 import Model.Id.Types
 import Model.Permission.Types
 import Model.Volume.Types
@@ -30,14 +29,3 @@ getShareFullDefault targetParty individualAccessLevel =
     nobodyId = (getPartyId . accountParty . siteAccount) nobodySiteAuth
     nobodyPublicLegacyDefault = Just True
     generalDefault = Nothing
-
-{-
-instance Has Volume VolumeAccess where
-  view = volumeAccessVolume
-instance Has (Id Volume) VolumeAccess where
-  view = (Has.view . volumeAccessVolume)
-instance Has Party VolumeAccess where
-  view = volumeAccessParty
-instance Has (Id Party) VolumeAccess where
-  view = (view . volumeAccessParty)
--}
