fa3bfc2cd696c33dcb10adedbc5eb261a662975a
Bryan Richter <bryan.richter@databrary.org>
2018-06-29 15:22:52 -0400

    Apply hlint and brittany to Model.Volume
    Some manual tweaking, as well.

--- src/Model/Volume.hs
+++ src/Model/Volume.hs
@@ -1,4 +1,9 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes, RecordWildCards, ScopedTypeVariables, DataKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Model.Volume
   ( module Model.Volume.Types
   , coreVolume
@@ -19,6 +24,7 @@ import Control.Applicative ((<|>))
 import Control.Monad (guard)
 import qualified Data.ByteString as BS
 import Data.List (find)
+import Data.Semigroup hiding ((<>))
 import Data.Monoid ((<>))
 import qualified Data.Time as TM
 import qualified Data.Text as T
@@ -27,7 +33,7 @@ import Database.PostgreSQL.Typed.Dynamic (pgLiteralRep)
 
 import Has (peek, view)
 import Service.DB
-import qualified JSON as JSON
+import qualified JSON
 import Model.SQL (selectQuery)
 import Model.Paginate (Paginate (..), def)
 import Model.Paginate.SQL (paginateSQL)
@@ -41,125 +47,171 @@ import Model.Volume.SQL
 import Model.VolumeAccess.Types (VolumeAccess(..))
 
 coreVolume :: Volume
-coreVolume = -- TODO: load on startup in lookups service module
-      Volume
-            (VolumeRow (Id 0) (T.pack "Core") Nothing Nothing Nothing)
-            (TM.UTCTime
-                (TM.ModifiedJulianDay 56303)
-                (TM.picosecondsToDiffTime 37600000000000000))
-            []
-            (RolePublicViewer PublicNoPolicy)
+-- TODO: load on startup in lookups service module
+coreVolume = Volume
+    (VolumeRow (Id 0) "Core" Nothing Nothing Nothing)
+    (TM.UTCTime
+        (TM.ModifiedJulianDay 56303)
+        (TM.picosecondsToDiffTime 37600000000000000)
+    )
+    []
+    (RolePublicViewer PublicNoPolicy)
 
-lookupVolume :: (MonadDB c m, MonadHasIdentity c m) => Id Volume -> m (Maybe Volume)
+lookupVolume
+    :: (MonadDB c m, MonadHasIdentity c m) => Id Volume -> m (Maybe Volume)
 lookupVolume vi = do
-  ident :: Identity <- peek
-  dbQuery1 $(selectQuery (selectVolume 'ident) "$WHERE volume.id = ${vi}")
+    ident :: Identity <- peek
+    dbQuery1 $(selectQuery (selectVolume 'ident) "$WHERE volume.id = ${vi}")
 
 changeVolume :: MonadAudit c m => Volume -> m ()
 changeVolume v = do
-  ident <- getAuditIdentity
-  dbExecute1' $(updateVolume 'ident 'v)
+    ident <- getAuditIdentity
+    dbExecute1' $(updateVolume 'ident 'v)
 
 addVolume :: MonadAudit c m => Volume -> m Volume
 addVolume bv = do
-  ident <- getAuditIdentity
-  dbQuery1' $ fmap (\v -> v [] RoleAdmin) $(insertVolume 'ident 'bv)
+    ident <- getAuditIdentity
+    dbQuery1' $ fmap (\v -> v [] RoleAdmin) $(insertVolume 'ident 'bv)
 
 getVolumeAlias :: Volume -> Maybe T.Text
-getVolumeAlias v = guard ((extractPermissionIgnorePolicy . volumeRolePolicy) v >= PermissionREAD) >> volumeAlias (volumeRow v)
+getVolumeAlias v = do
+    guard
+        (extractPermissionIgnorePolicy (volumeRolePolicy v) >= PermissionREAD)
+    volumeAlias (volumeRow v)
 
 auditVolumeDownload :: MonadAudit c m => Bool -> Volume -> m ()
 auditVolumeDownload success vol = do
-  ai <- getAuditIdentity
-  dbExecute1' [pgSQL|$INSERT INTO audit.volume (audit_action, audit_user, audit_ip, id) VALUES
-    (${if success then AuditActionOpen else AuditActionAttempt}, ${auditWho ai}, ${auditIp ai}, ${volumeId $ volumeRow vol})|]
+    ai <- getAuditIdentity
+    dbExecute1'
+        [pgSQL|$
+            INSERT INTO audit.volume (audit_action, audit_user, audit_ip, id)
+            VALUES (
+                ${if success then AuditActionOpen else AuditActionAttempt},
+                ${auditWho ai},
+                ${auditIp ai},
+                ${volumeId $ volumeRow vol})
+        |]
 
 volumeRowJSON :: JSON.ToObject o => VolumeRow -> JSON.Record (Id Volume) o
-volumeRowJSON VolumeRow{..} = JSON.Record volumeId $
-     "name" JSON..= volumeName
-  <> "body" JSON..= volumeBody
+volumeRowJSON VolumeRow {..} =
+    JSON.Record volumeId
+        $ "name"
+        JSON..= volumeName
+        <> "body"
+        JSON..= volumeBody
 
-volumeJSON :: JSON.ToObject o => Volume -> Maybe [VolumeAccess] -> JSON.Record (Id Volume) o
+volumeJSON
+    :: JSON.ToObject o
+    => Volume
+    -> Maybe [VolumeAccess]
+    -> JSON.Record (Id Volume) o
 volumeJSON v@Volume{..} mAccesses =
-    volumeRowJSON volumeRow `JSON.foldObjectIntoRec`
-   (
-       "doi" `JSON.kvObjectOrEmpty` volumeDOI volumeRow
-    <> "alias" `JSON.kvObjectOrEmpty` getVolumeAlias v
-    <> "creation" JSON..= volumeCreation
-    <> "owners" JSON..= map (\(i, n) -> JSON.Object $ "id" JSON..= i <> "name" JSON..= n) volumeOwners
-    <> "permission" JSON..= extractPermissionIgnorePolicy volumeRolePolicy
-    <> "publicsharefull" JSON..= volumeAccessPolicyJSON v
-    <> "publicaccess" `JSON.kvObjectOrEmpty` fmap (show . volumePublicAccessSummary) mAccesses)
+    JSON.foldObjectIntoRec
+        (volumeRowJSON volumeRow)
+        ("doi" `JSON.kvObjectOrEmpty` volumeDOI volumeRow
+        <> "alias" `JSON.kvObjectOrEmpty` getVolumeAlias v
+        <> "creation" JSON..= volumeCreation
+        <> "owners" JSON..=
+            map (\(i, n) -> JSON.Object ("id" JSON..= i <> "name" JSON..= n))
+                volumeOwners
+        <> "permission" JSON..= extractPermissionIgnorePolicy volumeRolePolicy
+        <> "publicsharefull" JSON..= volumeAccessPolicyJSON v
+        <> "publicaccess" `JSON.kvObjectOrEmpty`
+            fmap (show . volumePublicAccessSummary) mAccesses)
 
 volumeJSONSimple :: JSON.ToObject o => Volume -> JSON.Record (Id Volume) o
 volumeJSONSimple v = volumeJSON v Nothing
 
 volumeAccessPolicyJSON :: Volume -> Maybe Bool
-volumeAccessPolicyJSON v =
-  case volumeRolePolicy v of
+volumeAccessPolicyJSON v = case volumeRolePolicy v of
     RolePublicViewer PublicRestrictedPolicy -> Just False
     RoleSharedViewer SharedRestrictedPolicy -> Just False
     RolePublicViewer PublicNoPolicy -> Just True
     _ -> Nothing
 
 data VolumeFilter = VolumeFilter
-  { volumeFilterQuery :: Maybe String
-  , volumeFilterParty :: Maybe (Id Party)
-  , volumeFilterPaginate :: Paginate
-  }
+    { volumeFilterQuery :: Maybe String
+    , volumeFilterParty :: Maybe (Id Party)
+    , volumeFilterPaginate :: Paginate
+    }
+
+instance Semigroup VolumeFilter where
+    (<>) (VolumeFilter q1 p1 p) (VolumeFilter q2 p2 _) =
+        VolumeFilter (q1 <> q2) (p1 <|> p2) p
 
 instance Monoid VolumeFilter where
-  mempty = VolumeFilter Nothing Nothing def
-  mappend (VolumeFilter q1 p1 p) (VolumeFilter q2 p2 _) =
-    VolumeFilter (q1 <> q2) (p1 <|> p2) p
+    mempty = VolumeFilter Nothing Nothing def
+    mappend = (<>)
 
 volumeFilter :: VolumeFilter -> BS.ByteString
-volumeFilter VolumeFilter{..} = BS.concat
-  [ withq volumeFilterParty (const " JOIN volume_access ON volume.id = volume_access.volume")
-  , withq volumeFilterQuery (\n -> " JOIN volume_text_idx ON volume.id = volume_text_idx.volume, plainto_tsquery('english', " <> pgLiteralRep n <> ") query")
-  , " WHERE volume.id > 0 "
-  , withq volumeFilterParty (\p -> " AND volume_access.party = " <> pgLiteralRep p <> " AND volume_access.individual >= 'EDIT'")
-  , withq volumeFilterQuery (const " AND ts @@ query")
-  , " ORDER BY "
-  , withq volumeFilterQuery (const "ts_rank(ts, query) DESC,")
-  , withq volumeFilterParty (const "volume_access.individual DESC,")
-  , "volume.id DESC "
-  , paginateSQL volumeFilterPaginate
-  ]
-  where
-  withq v f = maybe BS.empty f v
+volumeFilter VolumeFilter {..} = BS.concat
+    [ withq
+        volumeFilterParty
+        (const " JOIN volume_access ON volume.id = volume_access.volume")
+    , withq
+        volumeFilterQuery
+        (\n ->
+            " JOIN volume_text_idx ON volume.id = volume_text_idx.volume, plainto_tsquery('english', "
+                <> pgLiteralRep n
+                <> ") query"
+        )
+    , " WHERE volume.id > 0 "
+    , withq
+        volumeFilterParty
+        (\p ->
+            " AND volume_access.party = "
+                <> pgLiteralRep p
+                <> " AND volume_access.individual >= 'EDIT'"
+        )
+    , withq volumeFilterQuery (const " AND ts @@ query")
+    , " ORDER BY "
+    , withq volumeFilterQuery (const "ts_rank(ts, query) DESC,")
+    , withq volumeFilterParty (const "volume_access.individual DESC,")
+    , "volume.id DESC "
+    , paginateSQL volumeFilterPaginate
+    ]
+    where withq v f = maybe BS.empty f v
 
 findVolumes :: (MonadHasIdentity c m, MonadDB c m) => VolumeFilter -> m [Volume]
 findVolumes pf = do
-  ident <- peek
-  dbQuery $ unsafeModifyQuery $(selectQuery (selectVolume 'ident) "")
-    (<> volumeFilter pf)
+    ident <- peek
+    dbQuery $ unsafeModifyQuery
+        $(selectQuery (selectVolume 'ident) "")
+        (<> volumeFilter pf)
 
 updateVolumeIndex :: MonadDB c m => m ()
-updateVolumeIndex =
-  dbExecute_ "SELECT volume_text_refresh()"
+updateVolumeIndex = dbExecute_ "SELECT volume_text_refresh()"
 
-data VolumePublicAccessLevel = PublicAccessFull | PublicAccessRestricted | PublicAccessNone deriving (Eq)
+data VolumePublicAccessLevel
+    = PublicAccessFull
+    | PublicAccessRestricted
+    | PublicAccessNone
+    deriving (Eq)
 
 instance Show VolumePublicAccessLevel where
-  show PublicAccessFull = "full"
-  show PublicAccessRestricted = "restricted"
-  show PublicAccessNone = "none"
+    show PublicAccessFull = "full"
+    show PublicAccessRestricted = "restricted"
+    show PublicAccessNone = "none"
 
 volumePublicAccessSummary :: [VolumeAccess] -> VolumePublicAccessLevel
-volumePublicAccessSummary vas =
-  maybe
+volumePublicAccessSummary vas = maybe
     PublicAccessNone
-    (\va ->
-       case volumeAccessChildren va of
-         PermissionNONE -> PublicAccessNone
-         PermissionPUBLIC ->
-           case toPolicyDefaulting (volumeAccessShareFull va) PublicNoPolicy PublicRestrictedPolicy of
-             PublicRestrictedPolicy -> PublicAccessRestricted
-             PublicNoPolicy -> PublicAccessFull
-         _ -> PublicAccessFull)
+    (\va -> case volumeAccessChildren va of
+        PermissionNONE -> PublicAccessNone
+        PermissionPUBLIC ->
+            case
+                    toPolicyDefaulting
+                        (volumeAccessShareFull va)
+                        PublicNoPolicy
+                        PublicRestrictedPolicy
+                of
+                    PublicRestrictedPolicy -> PublicAccessRestricted
+                    PublicNoPolicy -> PublicAccessFull
+        _ -> PublicAccessFull
+    )
     mPublicAccess
   where
     -- can't use equality on parties because Party is a circular type
-    mPublicAccess = find (\va -> (getPartyId . volumeAccessParty) va == nobodyId) vas
+    mPublicAccess =
+        find (\va -> (getPartyId . volumeAccessParty) va == nobodyId) vas
     nobodyId = getPartyId nobodyParty
