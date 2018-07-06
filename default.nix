09b907d8cec445677083c2014df56d2d73b21d57
kanishka <nish2575@gmail.com>
2018-07-05 20:20:19 -0400

    No ghcid for now, until we start using it actively

--- default.nix
+++ default.nix
@@ -91,7 +91,7 @@ let
           (drv.libraryHaskellDepends or [])
            ++
              (with self;
-               [ghcid cabal-install ghciDatabrary
+               [cabal-install ghciDatabrary
                # for ghci-databrary script
                wget cpio nodePackages.shell.nodeDependencies]);
       });
