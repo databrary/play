be1dd8131de32408111ca4a04b230d42fbe68c89
kanishka <nish2575@gmail.com>
2018-07-25 17:20:35 -0400

    nix serve passes the secret key file as a startup env variable instead
    - comment out unused nix.conf generation for cache server

--- setup-nix/cache-0-sudo.sh
+++ setup-nix/cache-0-sudo.sh
@@ -9,17 +9,17 @@ fi
 echo "Changing owner of /nix to $USER using sudo"
 sudo chown -R $USER /nix
 
-if ! [ -e /etc/nix ]; then
-    echo "Creating /etc/nix to house nix.conf using sudo"
-    sudo mkdir -p /etc/nix
-fi
-echo "Creating /etc/nix/nix.conf using sudo (overwriting any previous version)"
-if [ -e /etc/nix/nix.conf ]; then
-    echo "Backing up nix.conf"
-    sudo cp /etc/nix/nix.conf /etc/nix/nix.conf.backup
-fi
-# is this setting actually used? it appeared to use an incorrect path before
-sudo sh -c 'echo "secret-key-files = /home/datadeploy/devdatabrary2-nix-store.priv" > /etc/nix/nix.conf'
+# this might be cleaner than how nix-serve is currently started...
+# if ! [ -e /etc/nix ]; then
+#     echo "Creating /etc/nix to house nix.conf using sudo"
+#     sudo mkdir -p /etc/nix
+# fi
+# echo "Creating /etc/nix/nix.conf using sudo (overwriting any previous version)"
+# if [ -e /etc/nix/nix.conf ]; then
+#     echo "Backing up nix.conf"
+#     sudo cp /etc/nix/nix.conf /etc/nix/nix.conf.backup
+# fi
+# sudo sh -c 'echo "secret-key-files = /home/datadeploy/devdatabrary2-nix-store.priv" > /etc/nix/nix.conf'
 
 
 
