ca9b942b3dde258af5cca12312f0699acd4011f1
kanishka <nish2575@gmail.com>
2018-08-03 16:19:23 -0400

    Link various directories and files to allow tests to run in an isolated run directory
    - this is one step towards removing the sharing of database between running in ghci and tests

---
+++ runtime-dirs/databrary-unit-impure/unit-impure-run-tests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+# jsCssVersion.txt and empty web directory are still needed to start the backend
+# TODO: make flag to start backend only
+echo ":quit" | nix-shell --attr databrary-dev.env default.nix --command 'cd runtime-dirs/databrary-unit-impure && ../../ghci-databrary.sh'
+nix-shell --attr databrary-dev.env default.nix --command 'cd runtime-dirs/databrary-unit-impure && cabal -j test --test-options="--color always --hide-successes --timeout 2s"'
